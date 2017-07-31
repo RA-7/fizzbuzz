9efbca25a66e1d606a44e24696dbd98962d98117
RA-7 <klanecek.niko@gmail.com>
2017-07-31 11:01:00 +0200

    cleaned up adding the newline

--- wrong_fizzbuzz.py
+++ wrong_fizzbuzz.py
@@ -4,7 +4,7 @@ for i in range (1, 101):
         if i % 5 == 0:
             print('Buzz')
         else:
-            print('\n', end = '')
+            print()
     elif i % 5 == 0:
         if i % 3 == 0:
             print('Fizz', end = '')
