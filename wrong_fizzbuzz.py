2cf4349d320be0345c55d16535fe9047f6aaeb64
Niko Klaneček <klanecek.niko@gmail.com>
2017-08-01 13:22:11 +0200

    Added comments

--- wrong_fizzbuzz.py
+++ wrong_fizzbuzz.py
@@ -1,8 +1,12 @@
 for i in range (1, 101):
     if i % 3 == 0:
+        # End '' replaces the newline character
+        # that is normally at the end of every print
+        # so that Buzz can be added to the end of Fizz
         print('Fizz', end = '')
         if i % 5 == 0:
             print('Buzz')
+        # Add a newline at the end of Fizz if there is no Buzz
         else:
             print()
     elif i % 5 == 0:
